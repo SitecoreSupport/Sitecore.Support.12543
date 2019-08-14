0268d1aeec04f4729309dbd8fd8c00c821fdc6d6
Paul Stupka <paul.stupka@sitecore.com>
2019-08-14 19:15:26 +0300

    add SplitterRepository base implementation

--- src/Sitecore.Support.12543/RegisterDependencies.cs
+++ src/Sitecore.Support.12543/RegisterDependencies.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Sitecore.DependencyInjection;
+using Sitecore.XA.Feature.PageStructure.Repositories;
 using Sitecore.XA.Foundation.DynamicPlaceholders.Repositories;
 
 namespace Sitecore.Support
@@ -9,6 +10,7 @@ namespace Sitecore.Support
     public void Configure(IServiceCollection serviceCollection)
     {
       serviceCollection.AddTransient<IDynamicPlaceholdersRepository, Sitecore.Support.XA.Foundation.DynamicPlaceholders.Repositories.DynamicPlaceholdersRepository>();
+      serviceCollection.AddTransient<ISplitterRepository, Sitecore.Support.XA.Feature.PageStructure.Repositories.SplitterRepository>();
     }
   }
 }
---
+++ src/Sitecore.Support.12543/XA/Feature/PageStructure/Repositories/SplitterRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data.Items;
+using Sitecore.XA.Feature.PageStructure.Models;
+using Sitecore.XA.Foundation.Grid;
+using Sitecore.XA.Foundation.Grid.Model;
+using Sitecore.XA.Foundation.Grid.Parser;
+
+namespace Sitecore.Support.XA.Feature.PageStructure.Repositories
+{
+  public class SplitterRepository : Sitecore.XA.Feature.PageStructure.Repositories.SplitterRepository
+  {
+    private const string EnabledPlaceholders = "EnabledPlaceholders"; //because Sitecore.XA.Feature.PageStructure.Constants is internal
+    protected override List<string> GetColumnsStyles()
+    {
+      Item gridDefinitionItem = Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService<IGridContext>().GetGridDefinitionItem(Context.Item, Context.Device);
+
+      if (gridDefinitionItem == null)
+      {
+        return null;
+      }
+
+      GridDefinition gridDefinition = new GridDefinition(gridDefinitionItem);
+      IGridFieldParser parser = gridDefinition.InstantiateGridFieldParser();
+
+
+      var placeholders = new EnabledPlaceholders(Rendering.Parameters[EnabledPlaceholders]);
+
+      return placeholders.PlaceholdersIndexes.Select(col =>
+      {
+        col = col - 1;
+        string columnProperties = Rendering.Parameters[ColumnWidthParameterService.GetColumnWidthParameter(col)];
+        if (!string.IsNullOrWhiteSpace(columnProperties))
+        {
+          var classes = parser.ParseFromFieldValue(columnProperties).ToList();
+          if (classes.Any())
+          {
+            return string.Join(" ", classes);
+          }
+        }
+
+        return string.Empty;
+
+      }).ToList();
+    }
+  }
+}
