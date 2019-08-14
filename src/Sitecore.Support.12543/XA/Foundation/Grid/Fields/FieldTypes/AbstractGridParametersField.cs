afa3a149dfa34afc3627f0e56726954c67c30e67
Paul Stupka <paul.stupka@sitecore.com>
2019-08-14 15:31:11 +0300

    add AbstractGridParametersField base implementation

---
+++ src/Sitecore.Support.12543/XA/Foundation/Grid/Fields/FieldTypes/AbstractGridParametersField.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.XA.Foundation.Grid;
+using Sitecore.XA.Foundation.Grid.Fields.FieldRenderers;
+using Sitecore.XA.Foundation.Grid.Model;
+using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+using Sitecore.XA.Foundation.SitecoreExtensions.Services;
+
+namespace Sitecore.Support.XA.Foundation.Grid.Fields.FieldTypes
+{
+  public class AbstractGridParametersField : Sitecore.XA.Foundation.Grid.Fields.FieldTypes.AbstractGridParametersField
+  {
+    protected override IGridParametersFieldRenderer GridSettingsFieldRenderer()
+    {
+      ItemUri itemUri = GetItemUri();
+      var db = Data.Database.GetDatabase(itemUri.DatabaseName);
+      Item item = db.GetItem(itemUri.ItemID);
+      var deviceId = ServiceLocator.ServiceProvider.GetService<ISuspendedPipelineService>().GetCurrentDeviceId();
+      var device = db.GetItem(deviceId);
+
+      var gridDefinitionItem = ServiceLocator.ServiceProvider.GetService<IGridContext>().GetGridDefinitionItem(item, device);
+      if (gridDefinitionItem != null)
+      {
+        if (gridDefinitionItem.InheritsFrom(Templates.GridDefinition.ID))
+        {
+          return new GridDefinition(gridDefinitionItem).InstantiateFieldRenderer();
+        }
+        Log.Error($"Linked grid definition item with itemId:{gridDefinitionItem.ID} has incorrect template", this);
+        return null;
+      }
+      Log.Error($"Could not resolve grid definition item for itemId:{item?.ID}, device:{device?.ID}", this);
+      return null;
+    }
+  }
+}
