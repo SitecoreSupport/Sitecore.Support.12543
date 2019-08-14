d116cfc340dc19cd999103d8df3ae9a14c8572ac
Paul Stupka <paul.stupka@sitecore.com>
2019-08-14 13:38:10 +0300

    add SetGridValues base implementation

---
+++ src/Sitecore.Support.12543/XA/Foundation/Grid/Commands/SetGridValues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data;
+using Sitecore.Layouts;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.XA.Foundation.Grid;
+using Sitecore.XA.Foundation.Grid.Model;
+
+namespace Sitecore.Support.XA.Foundation.Grid.Commands
+{
+  public class SetGridValues : Sitecore.XA.Foundation.Grid.Commands.SetGridValues
+  {
+    protected override void FillGridParameters(CommandContext context, DeviceDefinition device, NameValueCollection parameters,
+      string fieldName)
+    {
+      var gridDefinitionItem = Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService<IGridContext>().GetGridDefinitionItem(context.Items.First(), Client.ContentDatabase.GetItem(device.ID));
+      var gridSettings = context.Parameters["gridSettings"].Split(new[]
+      {
+        '|'
+      }, StringSplitOptions.RemoveEmptyEntries).Select(ID.Parse).Where(id => !id.IsNull).ToList();
+
+      if (gridSettings.Any())
+      {
+        parameters[fieldName] = new GridDefinition(gridDefinitionItem).InstantiateGridFieldParser().ToFieldValue(gridSettings);
+      }
+      else
+      {
+        parameters[fieldName] = string.Empty;
+      }
+    }
+  }
+}
